77e92f915a6baa53b87841368a4d86aaafc993b6
Chien Le <lchien063@gmail.com>
2024-04-13 11:19:56 +0700

    -f

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import express from 'express'
-import cors from 'cors';
-import { getMetaData } from './metadata-scraper';
+import express from "express";
+import cors from "cors";
+import { getMetaData } from "./metadata-scraper";
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
+app.use("/fetch-metadata", async (req, res) => {
+  const data = await getMetaData(req.body.url);
+  res.send(data).status(200);
+});
 
-app.use('/fetch-metadata', async (req, res) => {
-    const data = await getMetaData(req.body.url)
-    res.send(data)
-})
-
-app.listen(8080, () => console.log('Listening on port 8080'))
+app.listen(8080, () => console.log("Listening on port 8080"));

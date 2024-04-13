caa3e0fef039f31cba4b84768a1ad7e7e8cfd61f
Chien Le <lchien063@gmail.com>
2024-04-13 22:58:58 +0700

    fix: user-agent

--- src/metadata-scraper.ts
+++ src/metadata-scraper.ts
@@ -1,20 +1,19 @@
 import * as ogs from "open-graph-scraper";
-const userAgent =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+import { USER_AGENT } from "./common.const";
 
 export const getMetaData = async (url: string) => {
   const data = await ogs
     .default({
       url,
       timeout: 5000,
-      fetchOptions: { headers: { "user-agent": userAgent } },
+      fetchOptions: { headers: { "user-agent": USER_AGENT } },
     })
     .then((metadata) => {
       console.log(metadata.result);
-      return {...metadata.result, url}
+      return { ...metadata.result, url };
     })
     .catch((error) => {
       console.log(error);
     });
-    return data
+  return data;
 };
